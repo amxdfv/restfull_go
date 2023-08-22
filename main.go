a49e0d9726a9d5a2cb465541a383d7a5662f7bca
Лёшка <amxdfv@yandex.ru>
2023-08-22 20:36:48 +0300

    Сделал скелет для 4-х запросов сервиса

--- main.go
+++ main.go
@@ -5,8 +5,21 @@ import (
 	"net/http"
 )
 
-func returnGet(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintln(w, "oslic")
+func returnGet(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("Пока не готово, но скоро будет"))
+	case http.MethodPost:
+		buf := make([]byte, 256)
+		r.Body.Read(buf)
+		fmt.Println(string(buf))
+		bodyResp := string(buf)
+		w.Write([]byte(bodyResp))
+	case http.MethodPut:
+		w.Write([]byte("Пока не готово, но скоро будет"))
+	case http.MethodDelete:
+		w.Write([]byte("Пока не готово, но скоро будет"))
+	}
 }
 
 func main() {
